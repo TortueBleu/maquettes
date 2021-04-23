8ed95aff69589c8239fdf690add0c97d2c928c38
TortueBleu <klarir.games@gmail.com>
2021-04-23 13:56:35 +0100

    ajout du js et des boutons pour afficher du text

---
+++ maquette1/maquette1.js
@@ -0,0 +1,25 @@
+let Who = document.getElementById("Who");
+let conso = document.getElementById("conso");
+let recolte = document.getElementById("recolte");
+let text = document.getElementById("text");
+
+
+Who.addEventListener("click", truc)
+conso.addEventListener("click", truc)
+recolte.addEventListener("click", truc)
+
+
+function truc(event) {
+console.log(event.target.id);
+    if (event.target.id === "Who") {
+        text.innerHTML = "Sed ut perspiciatis unde omnis iste  natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.";      
+    }
+    else if (event.target.id === "conso") {
+        
+        text.innerHTML = "Sed ut perspiciatis unde omnis iste  natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.";     
+    }
+    else if (event.target.id === "recolte") {
+       
+        text.innerHTML = "Sed ut perspiciatis unde omnis iste  natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.";
+    }
+}
